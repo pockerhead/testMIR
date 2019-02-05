f0912f31db417dff3a5f0f4be4fd5998b7ef974f
Артём Балашов <pockerhead@ya.ru>
2019-02-05 21:09:14 +0300

    Initial Commit

---
+++ testMIR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testMIR
+//
+//  Created by Артём Балашов on 05/02/2019.
+//  Copyright © 2019 pockerhead. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
